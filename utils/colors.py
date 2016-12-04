a07cb18cc8c1e1452a005293bfd817d230371886
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2016-12-03 22:51:13 -0200

    generate spaced colors for classes

---
+++ utils/colors.py
@@ -0,0 +1,13 @@
+static_colors = []
+
+def get_spaced_colors(n):
+    print("generating colors:" + str(n))
+    max_value = 16581375 #255**3
+    interval = int(max_value / n)
+    colors = [hex(I)[2:].zfill(6) for I in range(0, max_value, interval)]
+
+    return [(int(i[:2], 16), int(i[2:4], 16), int(i[4:], 16)) for i in colors]
+
+def gen_static_colors(n):
+    global static_colors
+    
