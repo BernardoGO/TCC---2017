667c48a508ab9b615b653cf46bce192d1c8af992
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2016-12-08 23:24:41 -0200

    basic config file

---
+++ config.py
@@ -0,0 +1,9 @@
+img_width, img_height = 500, 500
+sizex, sizey = 1936, 1296#3024,4032
+train_data_dir = 'data/train'
+validation_data_dir = 'data/validation'
+nb_train_samples = 150000
+nb_validation_samples = 24000
+epochs = 60
+batch_size = 16
+imagePath = "collection/Images/users/his109/grier_food_images/"
--- train.py
+++ train.py
@@ -1,6 +1,3 @@
 import utils.colors
 import logging as log
 import utils.basic
-
-utils.colors.get_spaced_colors(6)
-utils.colors.get_spaced_colors(6)
