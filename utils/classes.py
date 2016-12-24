f80a478cbf7f26c41ba5f4e03da2872bba82e106
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2016-12-24 00:13:06 -0200

    claspar_config in classes file

--- utils/classes.py
+++ utils/classes.py
@@ -5,5 +5,5 @@ import par_config
 def load_classes(filename=par_config.classes_pickle_file):
     with open(filename, 'rb') as handle:
         par_config.classes = pickle.load(handle)
-        log.info("Class Count: " + str(len(classes)))
-        log.info("Classes: " + str(classes))
+        log.info("Class Count: " + str(len(par_config.classes)))
+        log.info("Classes: " + str(par_config.classes))
