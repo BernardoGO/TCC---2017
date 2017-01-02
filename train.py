a489fe2e1bf32cab65fd8253a2fec66e5b2f6ae6
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2017-01-02 00:45:43 -0200

    supervis.csv + minor fixes

--- train.py
+++ train.py
@@ -1,3 +1,37 @@
+
+import par_config
+from PIL import ImageFont, ImageDraw
+from PIL import Image
+import time
+import numpy as np
+import core.model
+import core.roi_management
 import utils.colors
-import logging as log
-import utils.basic
+import utils.classes
+import data_management.dataset_io
+import copy
+
+print("Loading Data...")
+trainX, trainY = data_management.dataset_io.getData()
+
+
+# In[7]:
+
+
+ty = copy.deepcopy(trainY)
+ty = np.array(ty)
+ty = np.eye(25, dtype='uint8')[ty]
+#ty = ty.reshape((1,)+ty.shape)
+print("Fit...")
+batch_size = 32
+nb_epoch = 200
+
+ldModel.fit(np.array(trainX), np.array(ty),
+          batch_size=batch_size,
+          nb_epoch=nb_epoch,
+          validation_split=0.2,
+          callbacks=[tensorboard]
+         )
+
+ldModel.save_weights('image_500_retrIII.h5')
+save_model(ldModel, "model_500_retrIII.h5")
