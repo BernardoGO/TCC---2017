63004fde8c641c57dabc93fd280384ccef2d6740
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2017-01-07 01:02:44 -0200

    weights folder created

--- par_config.py
+++ par_config.py
@@ -3,12 +3,14 @@ sizex, sizey = 1936, 1296#3024,4032
 
 retrain_epochs = 200
 retrain_batch_size = 32
+
+weights_folder = "weights/"
 imagePath = "collection/Images/users/his109/grier_food_images/"
 
 
 dataset_csv_file = "supervis.csv"
 dataset_csv_path = ""
-classes_pickle_file = 'classesROI.pickle'
+classes_pickle_file = weights_folder+'classesROI.pickle'
 
 _CLASSES_TO_IGNORE_ = ["plate", "rag"]
 
@@ -19,7 +21,7 @@ test_image_name = "dsc_1601"
 images_extension = ".jpg"
 STRIDES = 1
 
-pretrained_weights_file = "image_500.h5"
+pretrained_weights_file =  weights_folder+"image_500.h5"
 
 joinRoIs_considered_coverage = 0.001
 final_optimizer = "rmsprop"
@@ -30,7 +32,9 @@ ignore_notJoined_boxes = True
 
 classify_batchsize = 64
 
-final_weights_filename = 'image_500_retr.h5'
-final_model_filename = "model_500_retr.h5"
+
+
+final_weights_filename = weights_folder+'image_500_retr.h5'
+final_model_filename =  weights_folder+"model_500_retr.h5"
 final_class_count = 25
 tensorboard_file = './Graph'
